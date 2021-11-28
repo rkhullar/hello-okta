702b5ff3c75ce60cd9d5efd1f5aacadd0f92bb93
Rajan Khullar <rkhullar03@gmail.com>
2021-11-28 14:12:01 -0500

    pre-render

---
+++ nextjs/pages/posts/index.jsx
@@ -0,0 +1,36 @@
+import { getSortedPostsData } from '../../lib/posts'
+import Layout from '../../components/layout'
+import utilStyles from '../../styles/utils.module.css'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <section>
+        <h2>Blog Posts Page</h2>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
