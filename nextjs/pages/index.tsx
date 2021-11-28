dfc8e6bde99105621bedfc10e4a15dc58fc9c8ca
Rajan Khullar <rkhullar03@gmail.com>
2021-11-28 11:55:14 -0500

    learning

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
@@ -13,7 +14,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className={styles.description}>
