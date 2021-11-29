58ab8bc98e97e29202348bde2d9fa9f732ce4578
Rajan Khullar <rkhullar03@gmail.com>
2021-11-29 17:05:28 -0500

    fix build errors; refactor okta provider again

--- frontend-nextjs/components/layout.tsx
+++ frontend-nextjs/components/layout.tsx
@@ -11,9 +11,9 @@ const Layout: React.FunctionComponent<Props> = ({ children, home }) => {
   const [session, loading] = useSession()
   let button
   if (session)
-    button = <button onClick={signOut}>Logout</button>
+    button = <button onClick={() => signOut()}>Logout</button>
   else
-    button = <button onClick={signIn}>Login</button>
+    button = <button onClick={() => signIn()}>Login</button>
   return (
     <>
     <Head>
--- frontend-nextjs/pages/api/auth/[...nextauth].ts
+++ frontend-nextjs/pages/api/auth/[...nextauth].ts
@@ -1,26 +1,25 @@
 import NextAuth from 'next-auth'
-import OktaProvider from 'next-auth/providers/okta'
+import Providers from 'next-auth/providers'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const options = {
-  providers: [
-    OktaProvider({
-      clientId: process.env.OKTA_CLIENT_ID,
-      clientSecret: process.env.OKTA_CLIENT_SECRET,
-      domain: `${process.env.OKTA_DOMAIN}/oauth2/default`
-    })
-  ],
-  callbacks: {
-    async jwt(token, user, account, profile, isNewUser) {
-      if (account?.accessToken)
-        token.accessToken = account.accessToken
-      return token
-    },
-    async session(session, token) {
-      session.accessToken = token.accessToken
-      return session
-    }
+const providers = [
+  Providers.Okta({
+    clientId: process.env.OKTA_CLIENT_ID,
+    clientSecret: process.env.OKTA_CLIENT_SECRET,
+    domain: `${process.env.OKTA_DOMAIN}/oauth2/default`
+  })
+]
+
+const callbacks = {
+  async jwt(token, user, account, profile, isNewUser) {
+    if (account?.accessToken)
+      token.accessToken = account.accessToken
+    return token
+  },
+  async session(session, token) {
+    session.accessToken = token.accessToken
+    return session
   }
 }
 
-export default (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, options)
+export default (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, {providers, callbacks})
