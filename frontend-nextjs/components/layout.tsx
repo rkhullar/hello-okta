32c87ae57e511be03c03665f74abd08cf6c3491e
Rajan Khullar <rkhullar03@gmail.com>
2021-11-29 11:56:35 -0500

    add profile stub page

--- frontend-nextjs/components/layout.tsx
+++ frontend-nextjs/components/layout.tsx
@@ -1,23 +1,39 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 interface Props {
   children: React.ReactNode
   home?: boolean
 }
 
-const Layout: React.FunctionComponent<Props> = ({ children, home }) => (
-  <>
+const Layout: React.FunctionComponent<Props> = ({ children, home }) => {
+  const [session, loading] = useSession();
+  let button;
+  if (session)
+    button = <button onClick={signOut}>Logout</button>
+  else
+    button = <button onClick={signIn}>Login</button>
+  return (
+    <>
     <Head>
       <link rel="icon" href="/favicon.ico" />
     </Head>
+    <section>
+      {!home && (
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      )}
+      {button}
+    </section>
+    <hr/>
     <main>{children}</main>
-    {!home && (
-      <Link href="/">
-        <a>← Back to home</a>
-      </Link>
-    )}
-  </>
-)
+    <hr/>
+    <footer>
+    </footer>
+    </>
+  )
+}
 
 export default Layout
---
+++ frontend-nextjs/pages/profile.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+export default function Profile() {
+  return (
+    <Layout>
+      <Head>
+        <title>Profile</title>
+      </Head>
+      <section>
+        <p>profile</p>
+      </section>
+    </Layout>
+  )
+}
