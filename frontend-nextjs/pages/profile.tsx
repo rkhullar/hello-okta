2678a7007e601259aca5f0ae08783a16ee55ded4
Rajan Khullar <rkhullar03@gmail.com>
2021-11-29 23:43:36 -0500

    remove relative imports

--- frontend-nextjs/pages/profile.tsx
+++ frontend-nextjs/pages/profile.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { useSession } from 'next-auth/client'
-import Layout from '../components/layout'
+import Layout from '$components/layout'
 
 export default function Profile() {
   const [session, loading] = useSession()
