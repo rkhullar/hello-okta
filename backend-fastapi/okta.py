603046bcd933859c0dd4072769f128ed6128a785
Rajan Khullar <rkhullar03@gmail.com>
2021-12-01 22:14:26 -0500

    trying with session flow

--- backend-fastapi/okta.py
+++ backend-fastapi/okta.py
@@ -25,3 +25,38 @@ class OktaClient:
         response = httpx.post(self.metadata['token_endpoint'], auth=(self.client_id, self.client_secret), data=payload)
         response.raise_for_status()
         return response.json()
+
+    ### wip
+
+    def authenticate(self, username: str, password: str) -> dict:
+        url = f'https://{self.domain}/api/v1/authn'
+        payload = dict(
+            username=username,
+            password=password,
+            options=dict(
+                multiOptionalFactorEnroll=False,
+                warnBeforePasswordExpired=False
+            )
+        )
+        response = httpx.post(url, json=payload)
+        response.raise_for_status()
+        return response.json()
+
+    def authorize(self, session_token: str) -> dict:
+        url = self.metadata['authorization_endpoint']
+        query_params = dict(
+            client_id=self.client_id,
+            scope='openid',
+            sessionToken=session_token,
+            state='ApplicationState',
+            response_type='code',
+            response_mode='query',
+            redirect_uri='http://localhost:8000/authorization-code/callback'
+        )
+        response = httpx.get(url, params=query_params)
+        print(response.text)
+
+    def request_token(self, username: str, password: str):
+        auth_n_data = self.authenticate(username=username, password=password)
+        auth_z_data = self.authorize(session_token=auth_n_data['sessionToken'])
+        print(auth_z_data)
