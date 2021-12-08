6934a2b7b184d10ac88edb5ec5e9c8616f237fc2
Rajan Khullar <rkhullar03@gmail.com>
2021-12-08 13:20:33 -0500

    make backend url dynamic

--- frontend-nextjs/pages/backend.tsx
+++ frontend-nextjs/pages/backend.tsx
@@ -3,8 +3,7 @@ import Layout from '$components/layout'
 import { useSession } from 'next-auth/react'
 import { useState } from 'react'
 
-async function loadProfile(session, callback) {
-  const base_url = 'http://0.0.0.0:8000'
+async function loadProfile(base_url: string, session, callback) {
   const response = await fetch(`${base_url}/profile`, {
     headers: {
       'Authorization': `Bearer ${session.access_token}`
@@ -14,14 +13,19 @@ async function loadProfile(session, callback) {
   callback(data)
 }
 
-async function loadHello(callback) {
-  const base_url = 'http://0.0.0.0:8000'
+async function loadHello(base_url: string, callback) {
   const response = await fetch(`${base_url}/hello`)
   const data = await response.json()
   callback(data)
 }
 
-export default function Backend() {
+export async function getServerSideProps() {
+  return {props: {
+    fastapi_url:  process.env.FASTAPI_URL
+  }}
+}
+
+export default function Backend({ fastapi_url }) {
   const { data: session } = useSession()
   const [count, setCount] = useState(0)
   const [result, setResult] = useState(null)
@@ -32,8 +36,8 @@ export default function Backend() {
       </Head>
       <section>
         <button onClick={() => setCount(count + 1)}>count={count}</button>
-        <button onClick={() => loadProfile(session, setResult)}>profile</button>
-        <button onClick={() => loadHello(setResult)}>hello</button>
+        <button onClick={() => loadProfile(fastapi_url, session, setResult)}>profile</button>
+        <button onClick={() => loadHello(fastapi_url, setResult)}>hello</button>
         <button onClick={() => setResult(null)}>clear</button>
         {result && (
           <p>{JSON.stringify(result)}</p>
