ccebbc6e296aceb983836734cc78470cb340e178
Rajan Khullar <rkhullar03@gmail.com>
2021-12-10 16:10:38 -0500

    save

--- frontend-nextjs/custom.js
+++ frontend-nextjs/custom.js
@@ -3,22 +3,58 @@
  * https://github.com/serverless-nextjs/serverless-next.js/pull/649
  */
 
+const AWS = require('aws-sdk')
 const originalLambda = require('./index')
-import { load_secret } from '$lib/secrets'
 
-// TODO: dynamically infer secret name somehow; should work with multiple environments
-const secret_name = 'serverless-poc-hello-sbx'
-const secret_data = load_secret(secret_name)
+// START - Secrets Lib
+const secrets_manager = new AWS.SecretsManager({region: 'us-east-1'});
+async function read_secret_arn(name: string) {
+  const filter = {Key: 'name', Values: [name]}
+  const response = await secrets_manager.listSecrets({MaxResults: 1, Filters: [filter]})
+  const secret_data = response.SecretList[0]
+  return secret_data.ARN
+}
 
-process.env.OKTA_DOMAIN = secret_data.okta_domain
-process.env.OKTA_CLIENT_ID = secret_data.okta_client_id
-process.env.OKTA_CLIENT_SECRET = secret_data.okta_client_secret
-process.env.NEXTAUTH_URL = secret_data.next_auth_url
-process.env.NEXTAUTH_SECRET = secret_data.next_auth_secret
-process.env.FASTAPI_URL = secret_data.fast_api_url
+async function load_secret_arn(arn: string) {
+  const response = await secrets_manager.getSecretValue({SecretId: arn})
+  return JSON.parse(response.SecretString)
+}
+
+async function load_secret(name: string) {
+  const arn = await read_secret_arn(name)
+  return await load_secret_arn(arn)
+}
+// END - Secrets Lib
+
+function set_env(key, val) {
+  process.env[key] = val
+}
+
+function inject_envs(data) {
+  // TODO: cleanup with loop?
+  set_env('OKTA_DOMAIN', data.okta_domain)
+  set_env('OKTA_DOMAIN', data.okta_domain)
+  set_env('OKTA_CLIENT_ID', data.okta_client_id)
+  set_env('OKTA_CLIENT_SECRET', data.okta_client_secret)
+  set_env('NEXTAUTH_URL', data.next_auth_url)
+  set_env('NEXTAUTH_SECRET', data.next_auth_secret)
+  set_env('FASTAPI_URL', data.fast_api_url)
+  // TODO: inject metadata: project and environment
+}
+
+let state_counter = 0
 
 exports.handler = async function(event, context) {
   console.log("inside custom handler")
+  console.log(context)
+
+  // TODO: dynamically infer secret name somehow; should work with multiple environments
+  const secret_name = 'serverless-poc-hello-sbx'
+  if (!state_counter) {
+    const secret_data = await load_secret(secret_name)
+    inject_envs(secret_data)
+  }
+
   context.callbackWaitsForEmptyEventLoop = false // TODO: verify needed
   return originalLambda.handler(event, context)
 }
--- frontend-nextjs/pages/api/hello.ts
+++ frontend-nextjs/pages/api/hello.ts
@@ -1,11 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-function setEnv(key: string, val: string) {
-  process.env[key] = val
-}
-
-// setEnv('TEST_ENV', 'dev')
-
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: 'hello world', environment: process.env.TEST_ENV })
+  res.status(200).json({ message: 'hello world', fastapi_url: process.env.FASTAPI_URL })
 }
