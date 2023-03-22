4d5f8bc01b5050ded3e26d0d9f6c7ae7cc001958
Rajan Khullar <rkhullar03@gmail.com>
2023-03-22 15:37:41 -0400

    upgrade python version; rename entrypoint

--- backend/fastapi/server.py
+++ backend/fastapi/server.py
@@ -34,4 +34,4 @@ settings = Settings()
 app = create_app(settings)
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host=settings.service_host, port=settings.service_port, reload=settings.debug)
+    uvicorn.run('server:app', host=settings.service_host, port=settings.service_port, reload=settings.debug)
