f1e3124b5ee974a0680716cf4b4c17b119469ef3
Rajan Khullar <rkhullar03@gmail.com>
2023-03-22 15:20:06 -0400

    reorg

--- frontend/nextjs-v1/pages/api/hello.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: 'hello world', fastapi_url: process.env.FASTAPI_URL })
-}
--- frontend/nextjs/pages/_app.tsx
+++ frontend/nextjs/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '$styles/globals.css'
+import { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // TODO: specify refetch interval? five hours?
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
